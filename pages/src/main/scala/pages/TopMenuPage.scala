9bf6151446725fea41dc94d5aa025e20e5d40241
cleverdeveloper <cleverdeveloper@github.com>
2018-10-02 13:58:10 +0300

    WIP: Click on frontend products.

--- pages/src/main/scala/pages/TopMenuPage.scala
+++ pages/src/main/scala/pages/TopMenuPage.scala
@@ -1,5 +1,7 @@
 package pages
 
+import TopMenuPage._
+import org.openqa.selenium.By
 import org.openqa.selenium.WebDriver
 import org.scalatest.selenium.WebBrowser._
 import testkit.WebPageUtils._
@@ -10,6 +12,11 @@ class TopMenuPage(implicit protected val webDriver: WebDriver) extends BrowserPa
   private val Register = linkText("Register")
   private val Logout = linkText("Logout")
   private val Login = linkText("Login")
+  
+  private def table =
+    xpath("//div[@id='content']//div[contains(@class,'product-thumb')]")
+  private def row(productName: String) =
+    xpath(s"${table.queryString}/")
 
   override def pageIsReady: Boolean = true
 
@@ -18,6 +25,20 @@ class TopMenuPage(implicit protected val webDriver: WebDriver) extends BrowserPa
     safeClick(Register)
     new RegistrationFormPage
   }
+  
+  def products: Seq[Product] =
+    tableRowsFromRootElement(table.element, By.tagName("div")) { element =>
+      Product(
+        url = element.findElement(By.xpath("/div[@class='image']/a")).getText,
+        name = element.findElement(By.xpath("/div[@class='caption']")).getText,
+        description = "",
+        price = "",
+        priceWithoutTax = ""
+      )
+    }
+  
+  private def addToCart(productName: String) =
+    xpath(s"${row(productName).queryString}//button[@data-original-title='Update']")
 
   def goToLoginPage(): AccountLoginPage = {
     safeClick(MyAccount)
@@ -34,8 +55,17 @@ class TopMenuPage(implicit protected val webDriver: WebDriver) extends BrowserPa
 }
 
 object TopMenuPage {
+  
   def prepare(implicit webDriver: WebDriver): TopMenuPage = {
     go to MainPage.url
     new TopMenuPage
   }
+  
+  case class Product(
+    url: String,
+    name: String,
+    description: String,
+    price: String,
+    priceWithoutTax: String
+  )
 }
