072ed1640a4d3bf584ba5f0c791ce345f20549df
Nikolajs Cmihuns <ncmihuns@evolutiongaming.com>
2018-10-02 10:17:58 +0300

    main trait to mix inside tests

---
+++ src/test/scala/testkit/AcceptanceOneBrowserSpec.scala
@@ -0,0 +1,33 @@
+package testkit
+
+import browsers.{ChromeExecutable, CustomizedChromeDriver}
+import org.openqa.selenium.WebDriver
+import org.scalatest._
+import org.scalatest.concurrent.Eventually
+import org.scalatestplus.play.{ChromeFactory, OneBrowserPerSuite}
+
+import scala.util.Try
+
+trait AcceptanceOneBrowserSpec extends WordSpec
+  with Eventually
+  with SpecPatienceConfig
+  with MustMatchers
+  with OptionValues
+  with GivenWhenThen
+  with BeforeAndAfterAll
+  with ChromeFactory
+  with ChromeExecutable
+  with OneBrowserPerSuite
+  with ScalaTestPlusPlay
+  with ScreenshotWhenOver
+  with BrowserLogWhenOver {
+
+  override def createWebDriver(): WebDriver = {
+    CustomizedChromeDriver.createWebDriver()
+  }
+
+  override def afterAll = {
+    Try(webDriver.quit())
+    super.afterAll()
+  }
+}
