5b86e831c718a42db1e822964863d578a626ed64
Nikolajs Cmihuns <ncmihuns@evolutiongaming.com>
2018-10-02 10:13:29 +0300

    custom chrome driver setup

---
+++ src/test/scala/browser/CustomizedChromeDriver.scala
@@ -0,0 +1,41 @@
+package browsers
+
+import java.util
+
+import io.github.bonigarcia.wdm.WebDriverManager
+import org.openqa.selenium.chrome.{ChromeDriver, ChromeOptions}
+import org.openqa.selenium.{UnexpectedAlertBehaviour, WebDriver}
+
+object CustomizedChromeDriver {
+
+  def createWebDriver(): WebDriver = {
+
+    val headlessChrome = sys.props.getOrElse("headless.chrome", "false").toBoolean
+    val devToolsRequired = sys.props.getOrElse("devtools.chrome", "false").toBoolean
+
+    val prefs = new util.HashMap[String, Boolean]
+    prefs.put("credentials_enable_service", false)
+    prefs.put("profile.password_manager_enabled", false)
+
+    val options = new ChromeOptions()
+
+    if (headlessChrome) {
+      options.addArguments(
+        "--headless",
+        "--disable-gpu",
+        "--window-size=1920x900"
+      )
+    }
+
+    if (devToolsRequired) {
+      options.addArguments("auto-open-devtools-for-tabs")
+    }
+
+    options
+      .addArguments("--disable-infobars", "--start-fullscreen")
+      .setExperimentalOption("prefs", prefs)
+      .setUnhandledPromptBehaviour(UnexpectedAlertBehaviour.ACCEPT)
+
+    new ChromeDriver(options)
+  }
+}
